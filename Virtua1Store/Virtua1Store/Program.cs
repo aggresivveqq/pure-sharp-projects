c807e7f4418c9a7a825de662977ed1b707aaef31
Chingiskhan <111143213+aggresivveqq@users.noreply.github.com>
2025-01-14 23:57:10 +0500

    Add files via upload

---
+++ Virtua1Store/Virtua1Store/Program.cs
@@ -0,0 +1,11 @@
+﻿﻿using VirtStore.repository;
+using VirtStore.service;
+using VirtStore.UI;
+
+var userRepository = new UserRepository();
+var userService = new UserService(userRepository);
+
+
+var mainMenu = new MainMenu(userService);
+
+mainMenu.Show();
---
+++ Virtua1Store/Virtua1Store/services/ProductService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using VirtStore.interfaces;
+using VirtStore.models;
+
+namespace VirtStore.services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        public ProductService(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void AddProduct(Product product)
+        {
+            var existingProduct = _productRepository.GetById(product.Id);
+            if (existingProduct != null)
+            {
+                throw new Exception("Продукт с таким ID уже существует.");
+            }
+
+            _productRepository.Add(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            var existingProduct = _productRepository.GetById(product.Id);
+            if (existingProduct == null)
+            {
+                throw new Exception("Продукт не найден.");
+            }
+
+            _productRepository.Update(product);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var existingProduct = _productRepository.GetById(id);
+            if (existingProduct == null)
+            {
+                throw new Exception("Продукт не найден.");
+            }
+
+            _productRepository.Delete(id);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _productRepository.GetAll();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _productRepository.GetById(id);
+        }
+
+        public List<Product> GetProductsByName(string name)
+        {
+            var products = _productRepository.GetAll();
+            return products.FindAll(p => p.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
